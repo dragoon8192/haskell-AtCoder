76f6e6899bb750664d64a245b607d3e97aec604d
dragoon8192_mac <dragoon8192@gmail.com>
2021-12-11 14:44:13 +0900

    new:   src/ABC049/C.hs

---
+++ src/ABC049/C.hs
@@ -0,0 +1,21 @@
+import Data.Maybe
+import Data.List
+import Control.Monad.Fix
+
+main =do
+  s <- getLine
+  if dream s
+  then putStrLn "Yes"
+  else putStrLn "No"
+
+gens = ["dream", "dreamer", "erase", "eraser"]
+step :: String -> [String]
+step ""  = [""]
+step str = mapMaybe (`stripPrefix` str) gens
+
+dream :: String -> Bool
+dream str = flip fix [str] $
+            \loop xs -> case xs of
+                "":_  -> True
+                []    -> False
+                _     -> loop . (step =<<) $ xs
