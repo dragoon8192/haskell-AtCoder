4ebb30c30a358e0cc8ab7e592419cfdaf5f663c0
dragoon8192_mac <dragoon8192@gmail.com>
2021-12-14 20:03:50 +0900

    update:   src/template.hs
    Use BS.getContents instead of BS.getLine.
    Add comments.

--- src/template.hs
+++ src/template.hs
@@ -2,50 +2,65 @@ import System.IO ( stdout, hFlush )
 import Control.Monad ( replicateM )
 import Data.Maybe ( fromJust )
 import qualified Data.ByteString.Char8 as BS
+--------------------------------
+-- /\ my template /\
+--------------------------------
 
+main = do
+  putStrLn "Hello, AtCoder!!"
+
+--------------------------------
+-- \/ my template \/
+--------------------------------
+{-#INLINE flush #-}
 flush :: IO ()
 flush = hFlush stdout
 
+{-#INLINE listToTuple #-}
 listToTuple :: (Integral a) => [a] -> (a,a)
 listToTuple (x: y: _) = (x, y)
 listToTuple xs = error $ "listToTuple error: " ++ show (map fromIntegral xs)
 
+{-#INLINE readInt #-}
 readInt :: (Integral a) => BS.ByteString -> a
 readInt = fromInteger . fst . fromJust . BS.readInteger
+{-#INLINE readIntTuple #-}
 readIntTuple :: (Integral a) => BS.ByteString -> (a, a)
 readIntTuple = listToTuple . map readInt . BS.words
+{-#INLINE readIntList #-}
 readIntList :: (Integral a) => BS.ByteString -> [a]
 readIntList = map readInt . BS.words
 
-getNBSs :: (Integral n) => n -> IO [BS.ByteString]
-getNBSs n = replicateM (fromIntegral n) BS.getLine
+{-#INLINE getBSsN #-}
+getBSsN :: (Integral n) => n -> IO [BS.ByteString]
+getBSsN n = take (fromIntegral n) . BS.lines <$> BS.getContents
 
+{-#INLINE getInt #-}
 getInt :: (Integral a) => IO a
 getInt = readInt <$> BS.getLine
+{-#INLINE getIntsN #-}
 getIntsN :: (Integral a, Integral n) => n -> IO [a]
-getIntsN n = map readInt <$> getNBSs n
+getIntsN n = map readInt <$> getBSsN n
+{-#INLINE getIntsAll #-}
 getIntsAll :: (Integral a) => IO [a]
 getIntsAll = map readInt . BS.lines <$> BS.getContents
 
+{-#INLINE getIntList #-}
 getIntList :: (Integral a) => IO [a]
 getIntList = readIntList <$> BS.getLine
+{-#INLINE getIntListsN #-}
 getIntListsN :: (Integral a, Integral n) => n -> IO [[a]]
-getIntListsN n = map readIntList <$> getNBSs n
+getIntListsN n = map readIntList <$> getBSsN n
+{-#INLINE getIntListsAll #-}
 getIntListsAll :: (Integral a) => IO [[a]]
 getIntListsAll = map readIntList . BS.lines <$> BS.getContents
 
+{-#INLINE getIntTuple #-}
 getIntTuple :: (Integral a) => IO (a, a)
 getIntTuple = readIntTuple <$> BS.getLine
+{-#INLINE getIntTuplesN #-}
 getIntTuplesN :: (Integral a, Integral n) => n -> IO [(a, a)]
-getIntTuplesN n = map readIntTuple <$> getNBSs n
+getIntTuplesN n = map readIntTuple <$> getBSsN n
+{-#INLINE getIntTuplesAll #-}
 getIntTuplesAll :: (Integral a) => IO [(a, a)]
 getIntTuplesAll = map readIntTuple . BS.lines <$> BS.getContents
-
-filterIOBS :: (BS.ByteString -> BS.ByteString) -> IO ()
-filterIOBS f = BS.putStrLn . f =<< BS.getLine
-filterIOInt :: (Integral a, Show b) => (a -> b) -> IO ()
-filterIOInt f = print . f =<< getInt
-filterIOIntsN :: (Integral a, Integral n, Show b) => n -> (a -> b) -> IO ()
-filterIOIntsN n f = do
-  replicateM (fromIntegral n) . filterIOInt $ f
-  return ()
