927dbcced12d5cc17a38a0741b6e5b7e20502dfd
dragoon8192_mac <dragoon8192@gmail.com>
2021-12-14 20:27:06 +0900

    update:   src/ABC231/D.hs
    TLE.

--- src/ABC231/D.hs
+++ src/ABC231/D.hs
@@ -1,18 +1,80 @@
+import System.IO ( stdout, hFlush )
+import Control.Monad ( replicateM )
+import Data.Maybe ( fromJust )
+import qualified Data.ByteString.Char8 as BS
+--------------------------------
+-- /\ my template /\
+--------------------------------
 import Data.List
+
 main = do
-  [n, m] <- map read . words <$> getLine :: IO [Integer]
-  abs <- map (map read . words) . lines <$> getContents :: IO [[Integer]]
+  (n, m) <- getIntTuple
+  abs <- getIntTuplesN m
   let xs = permutations [1..n]
   putStrLn $ if any (allneigh abs) xs
             then "Yes"
             else "No"
 
-neigh a b (x:y:xs) = ((a == x && b == y) || (a == y && b == x)) || neigh a b (y:xs)
-neigh _ _ [x] = False
-neigh _ _ [] = False
+neigh (a, b) (x:y:xs) = ((a == x && b == y) || (a == y && b == x)) || neigh (a, b) (y:xs)
+neigh _ [x] = False
+neigh _ [] = False
 
-allneigh :: [[Integer]] -> [Integer] -> Bool
-allneigh ([a,b]:abs) xs = if neigh a b xs
-                          then allneigh abs xs
-                          else False
+allneigh :: [(Integer, Integer)] -> [Integer] -> Bool
+allneigh (ab:abs) xs = neigh ab xs && allneigh abs xs
 allneigh [] _ = True
+
+--------------------------------
+-- \/ my template \/
+--------------------------------
+{-#INLINE flush #-}
+flush :: IO ()
+flush = hFlush stdout
+
+{-#INLINE listToTuple #-}
+listToTuple :: (Integral a) => [a] -> (a,a)
+listToTuple (x: y: _) = (x, y)
+listToTuple xs = error $ "listToTuple error: " ++ show (map fromIntegral xs)
+
+{-#INLINE readInt #-}
+readInt :: (Integral a) => BS.ByteString -> a
+readInt = fromInteger . fst . fromJust . BS.readInteger
+{-#INLINE readIntTuple #-}
+readIntTuple :: (Integral a) => BS.ByteString -> (a, a)
+readIntTuple = listToTuple . map readInt . BS.words
+{-#INLINE readIntList #-}
+readIntList :: (Integral a) => BS.ByteString -> [a]
+readIntList = map readInt . BS.words
+
+{-#INLINE getBSsN #-}
+getBSsN :: (Integral n) => n -> IO [BS.ByteString]
+getBSsN n = take (fromIntegral n) . BS.lines <$> BS.getContents
+
+{-#INLINE getInt #-}
+getInt :: (Integral a) => IO a
+getInt = readInt <$> BS.getLine
+{-#INLINE getIntsN #-}
+getIntsN :: (Integral a, Integral n) => n -> IO [a]
+getIntsN n = map readInt <$> getBSsN n
+{-#INLINE getIntsAll #-}
+getIntsAll :: (Integral a) => IO [a]
+getIntsAll = map readInt . BS.lines <$> BS.getContents
+
+{-#INLINE getIntList #-}
+getIntList :: (Integral a) => IO [a]
+getIntList = readIntList <$> BS.getLine
+{-#INLINE getIntListsN #-}
+getIntListsN :: (Integral a, Integral n) => n -> IO [[a]]
+getIntListsN n = map readIntList <$> getBSsN n
+{-#INLINE getIntListsAll #-}
+getIntListsAll :: (Integral a) => IO [[a]]
+getIntListsAll = map readIntList . BS.lines <$> BS.getContents
+
+{-#INLINE getIntTuple #-}
+getIntTuple :: (Integral a) => IO (a, a)
+getIntTuple = readIntTuple <$> BS.getLine
+{-#INLINE getIntTuplesN #-}
+getIntTuplesN :: (Integral a, Integral n) => n -> IO [(a, a)]
+getIntTuplesN n = map readIntTuple <$> getBSsN n
+{-#INLINE getIntTuplesAll #-}
+getIntTuplesAll :: (Integral a) => IO [(a, a)]
+getIntTuplesAll = map readIntTuple . BS.lines <$> BS.getContents
