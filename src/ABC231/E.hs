3612f20635daf3bcbdbaac8b4172b3178d008a59
dragoon8192_mac <dragoon8192@gmail.com>
2021-12-18 07:32:24 +0900

    update:   src/ABC231/E.hs

--- src/ABC231/E.hs
+++ src/ABC231/E.hs
@@ -1,10 +1,88 @@
-main = do
-  [n,x] <- map read . words <$> getLine
-  as <- map read . words <$> getLine
-  let ps = f as
-  let aps = zip as ps
-    where
-      f [] = []
-      f [a] = div x a + 2
-      f (a0: a1: as) = div a1 a0 : f as
+{-# LANGUAGE MultiWayIf #-}
 
+import System.IO ( stdout, hFlush )
+import Control.Monad ( replicateM )
+import Data.Maybe ( fromJust )
+import qualified Data.ByteString.Char8 as BS
+--------------------------------
+-- /\ my template /\
+--------------------------------
+
+main = do
+  (n,x) <- getIntTuple
+  as <- getIntList
+  let bs = ratioList as
+  print $ coinNum x bs
+
+ratioList []  = []
+ratioList [_] = []
+ratioList (a0: as@(a1:_)) = div a1 a0 : ratioList as
+
+coinList y []= [y]
+coinList y (b0: bs) = c0: coinList y' bs
+  where
+    (y', c0) = divMod y b0
+
+coinNum x bs = coinNumFunc (coinList x bs) bs
+  where
+    coinNumFunc (c0: cs@(c1: cs')) (b0: bs) = if
+      | c0 <= 1 + div b0 2 -> c0 + coinNumFunc cs bs
+      | otherwise      -> (b0 - c0) + coinNumFunc ((c1 + 1): cs') bs
+    coinNumFunc (c0:_) [] = c0
+    coinNumFunc _ _ = undefined
+
+--------------------------------
+-- \/ my template \/
+--------------------------------
+{-#INLINE flush #-}
+flush :: IO ()
+flush = hFlush stdout
+
+{-#INLINE listToTuple #-}
+listToTuple :: (Integral a) => [a] -> (a,a)
+listToTuple (x: y: _) = (x, y)
+listToTuple xs = error $ "listToTuple error: " ++ show (map fromIntegral xs)
+
+{-#INLINE readInt #-}
+readInt :: (Integral a) => BS.ByteString -> a
+readInt = fromInteger . fst . fromJust . BS.readInteger
+{-#INLINE readIntTuple #-}
+readIntTuple :: (Integral a) => BS.ByteString -> (a, a)
+readIntTuple = listToTuple . map readInt . BS.words
+{-#INLINE readIntList #-}
+readIntList :: (Integral a) => BS.ByteString -> [a]
+readIntList = map readInt . BS.words
+
+{-#INLINE getBSsN #-}
+getBSsN :: (Integral n) => n -> IO [BS.ByteString]
+getBSsN n = take (fromIntegral n) . BS.lines <$> BS.getContents
+
+{-#INLINE getInt #-}
+getInt :: (Integral a) => IO a
+getInt = readInt <$> BS.getLine
+{-#INLINE getIntsN #-}
+getIntsN :: (Integral a, Integral n) => n -> IO [a]
+getIntsN n = map readInt <$> getBSsN n
+{-#INLINE getIntsAll #-}
+getIntsAll :: (Integral a) => IO [a]
+getIntsAll = map readInt . BS.lines <$> BS.getContents
+
+{-#INLINE getIntList #-}
+getIntList :: (Integral a) => IO [a]
+getIntList = readIntList <$> BS.getLine
+{-#INLINE getIntListsN #-}
+getIntListsN :: (Integral a, Integral n) => n -> IO [[a]]
+getIntListsN n = map readIntList <$> getBSsN n
+{-#INLINE getIntListsAll #-}
+getIntListsAll :: (Integral a) => IO [[a]]
+getIntListsAll = map readIntList . BS.lines <$> BS.getContents
+
+{-#INLINE getIntTuple #-}
+getIntTuple :: (Integral a) => IO (a, a)
+getIntTuple = readIntTuple <$> BS.getLine
+{-#INLINE getIntTuplesN #-}
+getIntTuplesN :: (Integral a, Integral n) => n -> IO [(a, a)]
+getIntTuplesN n = map readIntTuple <$> getBSsN n
+{-#INLINE getIntTuplesAll #-}
+getIntTuplesAll :: (Integral a) => IO [(a, a)]
+getIntTuplesAll = map readIntTuple . BS.lines <$> BS.getContents
