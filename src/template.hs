0faf62adfe237e5eb8399764f3eb3fa2fdeeb03e
dragoon8192_mac <dragoon8192@gmail.com>
2021-12-25 20:33:58 +0900

    update:   src/template.hs
    Add printYesNo.

--- src/template.hs
+++ src/template.hs
@@ -1,4 +1,4 @@
-{-#LANGUAGE MultiWayIf #-}
+{-# LANGUAGE MultiWayIf #-}
 import System.IO ( stdout, hFlush )
 import Control.Monad ( replicateM )
 import Data.Maybe ( fromJust )
@@ -13,56 +13,65 @@ main = do
 --------------------------------
 -- \/ my template \/
 --------------------------------
-{-#INLINE flush #-}
+{-# INLINE flush #-}
 flush :: IO ()
 flush = hFlush stdout
 
-{-#INLINE listToTuple #-}
+{-# INLINE listToTuple #-}
 listToTuple :: (Integral a) => [a] -> (a,a)
 listToTuple (x: y: _) = (x, y)
 listToTuple xs        = error $ "listToTuple error: " ++ show (map fromIntegral xs)
 
-{-#INLINE readInt #-}
+{-# INLINE readInt #-}
 readInt :: (Integral a) => BS.ByteString -> a
 readInt = fromInteger . fst . fromJust . BS.readInteger
-{-#INLINE readIntTuple #-}
+{-# INLINE readIntTuple #-}
 readIntTuple :: (Integral a) => BS.ByteString -> (a, a)
 readIntTuple = listToTuple . map readInt . BS.words
-{-#INLINE readIntList #-}
+{-# INLINE readIntList #-}
 readIntList :: (Integral a) => BS.ByteString -> [a]
 readIntList = map readInt . BS.words
 
-{-#INLINE getBSsN #-}
+{-# INLINE getBSsN #-}
 getBSsN :: (Integral n) => n -> IO [BS.ByteString]
 getBSsN n = replicateM (fromIntegral n) BS.getLine
 -- getBSsN n = take (fromIntegral n) . BS.lines <$> BS.getContents
 
-{-#INLINE getInt #-}
+{-# INLINE getInt #-}
 getInt :: (Integral a) => IO a
 getInt = readInt <$> BS.getLine
-{-#INLINE getIntsN #-}
+{-# INLINE getIntsN #-}
 getIntsN :: (Integral a, Integral n) => n -> IO [a]
 getIntsN n = map readInt <$> getBSsN n
-{-#INLINE getIntsAll #-}
+{-# INLINE getIntsAll #-}
 getIntsAll :: (Integral a) => IO [a]
 getIntsAll = map readInt . BS.lines <$> BS.getContents
 
-{-#INLINE getIntList #-}
+{-# INLINE getIntList #-}
 getIntList :: (Integral a) => IO [a]
 getIntList = readIntList <$> BS.getLine
-{-#INLINE getIntListsN #-}
+{-# INLINE getIntListsN #-}
 getIntListsN :: (Integral a, Integral n) => n -> IO [[a]]
 getIntListsN n = map readIntList <$> getBSsN n
-{-#INLINE getIntListsAll #-}
+{-# INLINE getIntListsAll #-}
 getIntListsAll :: (Integral a) => IO [[a]]
 getIntListsAll = map readIntList . BS.lines <$> BS.getContents
+{-# INLINE getIntMatrix #-}
+getIntMatrix = getIntListsAll
 
-{-#INLINE getIntTuple #-}
+{-# INLINE getIntTuple #-}
 getIntTuple :: (Integral a) => IO (a, a)
 getIntTuple = readIntTuple <$> BS.getLine
-{-#INLINE getIntTuplesN #-}
+{-# INLINE getIntTuplesN #-}
 getIntTuplesN :: (Integral a, Integral n) => n -> IO [(a, a)]
 getIntTuplesN n = map readIntTuple <$> getBSsN n
-{-#INLINE getIntTuplesAll #-}
+{-# INLINE getIntTuplesAll #-}
 getIntTuplesAll :: (Integral a) => IO [(a, a)]
 getIntTuplesAll = map readIntTuple . BS.lines <$> BS.getContents
+
+{-# INLINE printYesNo #-}
+printYesNo :: Bool -> IO ()
+printYesNo bool = print $ if bool then "Yes" else "No"
+{-# INLINE printYESNO #-}
+printYESNO :: Bool -> IO ()
+printYESNO bool = print $ if bool then "YES" else "NO"
