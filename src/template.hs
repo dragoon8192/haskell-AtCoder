4547a7cc28387b9f9f7da4b5a764d11c9b33360c
dragoon8192_mac <dragoon8192@gmail.com>
2022-01-12 18:47:08 +0900

    update:   src/template.hs Use Attoparsec.

--- src/template.hs
+++ src/template.hs
@@ -1,13 +1,17 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE NegativeLiterals #-}
+{-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE TupleSections, MultiWayIf #-}
-
-import Prelude hiding (sum, product)
-import System.IO ( stdout, hFlush )
+-- For template functions.
+import Prelude hiding ( sum, product )
+import System.IO ( stdin, stdout, hFlush )
 import Control.Monad ( replicateM )
 import Data.Maybe ( fromJust )
+import Control.Monad.State
+import Control.Applicative
 import qualified Data.ByteString.Char8 as BS
-
+import qualified Data.Attoparsec.ByteString.Char8 as AP
+-- For main.
 import Debug.Trace
 import Data.Set (Set)
 import Data.Map.Strict (Map)
@@ -23,24 +27,53 @@ import qualified Data.IntMap.Strict as IM
 -- /\ my template /\
 --------------------------------
 
-main = do
-  putStrLn "Hello, AtCoder!!"
+main = runSolver do
+  lift $ putStrLn "Hello, AtCoder!!"
+  a <- parseLine $ spaceSepTuple int int
+  lift $ print a
+  -- b <- parseLine $ spaceSepList int
+  -- lift $ print b
 
 --------------------------------
 -- \/ my template \/
 --------------------------------
 
--- fixed Prelude
+-- Solver
+type Solver = StateT BS.ByteString IO
 
-{-# INLINE sum #-}
-sum :: (Foldable t, Num a) => t a -> a
-sum = foldr (+) 0
-{-# INLINE product #-}
-product :: (Foldable t, Num a) => t a -> a
-product = foldr (*) 0
+runSolver :: Solver a -> IO a
+runSolver s = evalStateT s BS.empty
+
+-- Parser
+parse :: AP.Parser a -> Solver a
+parse parser = do
+  result <- AP.parseWith getSome parser =<< get
+  case result of
+    AP.Fail _ list errMsg -> error errMsg
+    AP.Partial _          -> error "error: Partial"
+    AP.Done input a       -> do
+      put input
+      return a
+  where
+    getSome = lift $ BS.hGetSome stdin 65535
+{-# INLINE parseLine #-}
+parseLine :: AP.Parser a -> Solver a
+parseLine parser = parse $ parser <* AP.endOfLine
+{-# INLINE spaceOrTab #-}
+spaceOrTab :: AP.Parser Char
+spaceOrTab = AP.char ' ' <|> AP.char '\t'
+  -- AP.space は \n も含む
+{-# INLINE spaceSepList #-}
+spaceSepList :: AP.Parser a -> AP.Parser [a]
+spaceSepList = flip AP.sepBy $ AP.many1 spaceOrTab
+{-# INLINE spaceSepTuple #-}
+spaceSepTuple :: AP.Parser a -> AP.Parser b -> AP.Parser (a,b)
+spaceSepTuple pa pb = (,) <$> pa <* AP.many1 spaceOrTab <*> pb
+{-# INLINE int #-}
+int :: (Integral a) => AP.Parser a
+int = AP.signed AP.decimal
 
 -- IO
-
 {-# INLINE flush #-}
 flush :: IO ()
 flush = hFlush stdout
@@ -103,3 +136,11 @@ printYesNo bool = print $ if bool then "Yes" else "No"
 {-# INLINE printYESNO #-}
 printYESNO :: Bool -> IO ()
 printYESNO bool = print $ if bool then "YES" else "NO"
+
+-- fixed Prelude
+{-# INLINE sum #-}
+sum :: (Foldable t, Num a) => t a -> a
+sum = foldr (+) 0
+{-# INLINE product #-}
+product :: (Foldable t, Num a) => t a -> a
+product = foldr (*) 0
