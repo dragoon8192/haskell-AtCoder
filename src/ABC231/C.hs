2f5681b9c65643e5937e70bdfbee76ba46286941
dragoon8192_mac <dragoon8192@gmail.com>
2022-01-14 20:39:16 +0900

    update:   src/ABC231/C.hs
    Use new template.

--- src/ABC231/C.hs
+++ src/ABC231/C.hs
@@ -1,62 +1,131 @@
-import System.IO ( stdout, hFlush )
-import Control.Monad ( replicateM )
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE NegativeLiterals, OverloadedStrings #-}
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE TupleSections, MultiWayIf #-}
+
+-- For template functions.
+import Prelude hiding ( sum, product )
+import System.IO ( stdin, stdout, hFlush )
 import Data.Maybe ( fromJust )
-import qualified Data.ByteString.Char8 as BS
+import Data.Either ( fromLeft )
+import Data.Bool ( bool )
+import Control.Monad ( replicateM )
+import Control.Monad.State
+    ( MonadTrans(lift), StateT, MonadState(put, get), evalStateT )
+import Control.Applicative ( Alternative((<|>)) )
+import qualified Data.ByteString.Char8            as BS
+import qualified Data.Attoparsec.ByteString.Char8 as AP
 
-import Data.List
-import qualified Data.IntMap as IM
+-- For main.
+import Debug.Trace ()
+import Data.Set        (Set)
+import Data.Map.Strict (Map)
+import Data.IntSet     (IntSet)
+import Data.IntMap     (IntMap)
+import qualified Data.List          as L
+import qualified Data.Set           as S
+import qualified Data.Map.Strict    as M
+import qualified Data.IntSet        as IS
+import qualified Data.IntMap.Strict as IM
 
+--------------------------------
+-- /\ my template /\
+--------------------------------
+
+main :: IO ()
+main = runSolver do
+  (n, q) <- parseLine $ spaceSepTuple int int
+  as <- IM.fromListWith (+) . flip zip (repeat 1)
+    <$> (parseLine . spaceSepList $ int)
+  let gtEqNums = snd . IM.mapAccum (\acc eqNum -> (acc + eqNum, n - acc)) 0 $ as
+  let gtEqCount x = maybe 0 snd . IM.lookupGE x $ gtEqNums
+  xs <- parseLinesN q int
+  lift $ putStr . unlines . map (show . gtEqCount) $ xs
+
+--------------------------------
+-- \/ my template \/
+--------------------------------
+
+-- Solver
+type Solver = StateT BS.ByteString IO
+
+runSolver :: Solver a -> IO a
+runSolver s = evalStateT s BS.empty
+
+-- Parser
+parse :: AP.Parser a -> Solver a
+parse parser = do
+  result <- AP.parseWith getSome parser =<< get
+  case result of
+    AP.Done input a -> put input >> return a
+    _               -> error . fromLeft "" . AP.eitherResult $ result
+  where
+    getSome = lift $ BS.hGetSome stdin 65535
+
+{-# INLINE parseLine #-}
+parseLine :: AP.Parser a -> Solver a
+parseLine parser = parse $ parser <* AP.endOfLine
+
+{-# INLINE parseLinesN #-}
+parseLinesN :: (Integral n) => n -> AP.Parser a -> Solver [a]
+parseLinesN n parser = parse . AP.count (fromIntegral n) $ parser <* AP.endOfLine
+
+{-# INLINE spaceOrTab #-}
+spaceOrTab :: AP.Parser Char
+spaceOrTab = AP.char ' ' <|> AP.char '\t'
+  -- AP.space は \n も含む
+
+{-# INLINE spaceSepList #-}
+spaceSepList :: AP.Parser a -> AP.Parser [a]
+spaceSepList = flip AP.sepBy $ AP.many1 spaceOrTab
+
+{-# INLINE spaceSepTuple #-}
+spaceSepTuple :: AP.Parser a -> AP.Parser b -> AP.Parser (a, b)
+spaceSepTuple pa pb = (,) <$> pa <* AP.many1 spaceOrTab
+                          <*> pb
+
+{-# INLINE spaceSepTuple3 #-}
+spaceSepTuple3 :: AP.Parser a -> AP.Parser b -> AP.Parser c -> AP.Parser (a, b, c)
+spaceSepTuple3 pa pb pc = (,,)  <$> pa <* AP.many1 spaceOrTab
+                                <*> pb <* AP.many1 spaceOrTab
+                                <*> pc
+
+{-# INLINE int #-}
+int :: (Integral a) => AP.Parser a
+int = AP.signed AP.decimal
+
+{-# INLINE string #-}
+string :: AP.Parser BS.ByteString
+string = AP.takeTill AP.isSpace
+
+{-# INLINE hashDotToBool #-}
+hashDotToBool :: Char -> Bool
+hashDotToBool = (== '#')
+
+{-# INLINE boolToHashDot #-}
+boolToHashDot :: Bool -> Char
+boolToHashDot = bool '#' '.'
+
+-- IO
+
+{-# INLINE flush #-}
 flush :: IO ()
 flush = hFlush stdout
 
-listToTuple :: (Integral a) => [a] -> (a,a)
-listToTuple (x: y: _) = (x, y)
-listToTuple xs = error $ "listToTuple error: " ++ show (map fromIntegral xs)
+{-# INLINE printYesNo #-}
+printYesNo :: Bool -> IO ()
+printYesNo bool = print $ if bool then "Yes" else "No"
 
-readInt :: (Integral a) => BS.ByteString -> a
-readInt = fromInteger . fst . fromJust . BS.readInteger
-readIntTuple :: (Integral a) => BS.ByteString -> (a, a)
-readIntTuple = listToTuple . map readInt . BS.words
-readIntList :: (Integral a) => BS.ByteString -> [a]
-readIntList = map readInt . BS.words
+{-# INLINE printYESNO #-}
+printYESNO :: Bool -> IO ()
+printYESNO bool = print $ if bool then "YES" else "NO"
 
-getBSsN :: (Integral n) => n -> IO [BS.ByteString]
-getBSsN n = take (fromIntegral n) . BS.lines <$> BS.getContents
+-- fixed Prelude
 
-getInt :: (Integral a) => IO a
-getInt = readInt <$> BS.getLine
-getIntsN :: (Integral a, Integral n) => n -> IO [a]
-getIntsN n = map readInt <$> getBSsN n
-getIntsAll :: (Integral a) => IO [a]
-getIntsAll = map readInt . BS.lines <$> BS.getContents
+{-# INLINE sum #-}
+sum :: (Foldable t, Num a) => t a -> a
+sum = foldr (+) 0
 
-getIntList :: (Integral a) => IO [a]
-getIntList = readIntList <$> BS.getLine
-getIntListsN :: (Integral a, Integral n) => n -> IO [[a]]
-getIntListsN n = map readIntList <$> getBSsN n
-getIntListsAll :: (Integral a) => IO [[a]]
-getIntListsAll = map readIntList . BS.lines <$> BS.getContents
-
-getIntTuple :: (Integral a) => IO (a, a)
-getIntTuple = readIntTuple <$> BS.getLine
-getIntTuplesN :: (Integral a, Integral n) => n -> IO [(a, a)]
-getIntTuplesN n = map readIntTuple <$> getBSsN n
-getIntTuplesAll :: (Integral a) => IO [(a, a)]
-getIntTuplesAll = map readIntTuple . BS.lines <$> BS.getContents
-
--- filterIOBS :: (BS.ByteString -> BS.ByteString) -> IO ()
--- filterIOBS f = BS.putStrLn . f =<< BS.getLine
--- filterIOInt :: (Integral a, Show b) => (a -> b) -> IO ()
--- filterIOInt f = print . f =<< getInt
--- filterIOIntsN :: (Integral a, Integral n, Show b) => n -> (a -> b) -> IO ()
--- filterIOIntsN n f = do
---   replicateM (fromIntegral n) . filterIOInt $ f
---   return ()
-
-main = do
-  (n, q) <- getIntTuple
-  as <- IM.fromListWith (+) . flip zip (repeat 1) <$> getIntList
-  let gtEqNums = snd . IM.mapAccum (\acc eqNum -> (acc + eqNum, n - acc)) 0 $ as
-  let gtEqCount x = maybe 0 snd . IM.lookupGE x $ gtEqNums
-  xs <- getIntsN q
-  putStr . unlines . map (show . gtEqCount) $ xs
+{-# INLINE product #-}
+product :: (Foldable t, Num a) => t a -> a
+product = foldr (*) 0
